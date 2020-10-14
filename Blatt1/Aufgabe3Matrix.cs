44ba4c21c829038f55926ed71a4e425c19ea0913
Christoph Hilmer <hilmerchristoph@googlemail.com>
2020-10-14 21:10:19 +0200

    Add project files.

---
+++ Blatt1/Aufgabe3Matrix.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Blatt1
+{
+    public class Aufgabe3Matrix
+    {
+        public int M { get; private set; }
+        public int N { get; private set; }
+        public int[,] Matrix { get; set; }
+
+        public TimeSpan Laufzeit { get; set; }
+        public int Schritte { get; set; }
+
+        public Aufgabe3Matrix(int m, int n)
+        {
+            M = m;
+            N = n;
+            Matrix = new int[m, n];
+        }
+
+        public void Init()
+        {
+            Matrix = new int[M, N];
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write($"{Matrix[i, j]} \t");
+
+                    if (j < N-1)
+                    {
+                        Console.Write('|');
+                    }
+                }
+
+                Console.WriteLine();
+                for (int j = 0; j < 8 * N; j++)
+                {
+                    Console.Write('-');
+                }
+                Console.WriteLine();
+
+            }
+        }
+
+        public void RandomFill()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Matrix[i, j] = rnd.Next(100);
+                }
+            }
+        }
+
+        public Aufgabe3Matrix Add(Aufgabe3Matrix b)
+        {
+            Stopwatch s = new Stopwatch();
+            s.Start();
+            Schritte = 0;
+            if (b.M != M || b.N != N)
+            {
+                throw new Exception("Kann nicht addiert werden.");
+            }
+            Aufgabe3Matrix erg = new Aufgabe3Matrix(M, N);
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Schritte++;
+                    erg.Matrix[i, j] = Matrix[i, j] + b.Matrix[i, j];
+                }
+            }
+            s.Stop();
+            Laufzeit = s.Elapsed;
+            b.Laufzeit = s.Elapsed;
+            b.Schritte = Schritte;
+
+            return erg;
+        }
+
+        public Aufgabe3Matrix Mult(Aufgabe3Matrix b)
+        {
+            Stopwatch s = new Stopwatch();
+            s.Start();
+            Schritte = 0;
+            if (N != b.M)
+            {
+                throw new Exception("Kann nicht Multipliziert werden.");
+            }
+            Aufgabe3Matrix erg = new Aufgabe3Matrix(M, b.N);
+            for (int i = 0; i < b.N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    for (int k = 0; k < N; k++)
+                    {
+                        Schritte++;
+                        erg.Matrix[j, i] += Matrix[j, k] * b.Matrix[k, i];
+                    }
+                }
+            }
+            s.Stop();
+            Laufzeit = s.Elapsed;
+            b.Laufzeit = s.Elapsed;
+            b.Schritte = Schritte;
+
+            return erg;
+        }
+
+        public static int LaufzeitErmittlungAdd(int minuten, int initGroesse)
+        {
+            int intervall = 1000;
+            Aufgabe3Matrix a;
+            Aufgabe3Matrix b;
+
+            do
+            {
+                a = new Aufgabe3Matrix(initGroesse, initGroesse);
+                a.RandomFill();
+
+                b = new Aufgabe3Matrix(initGroesse, initGroesse);
+                b.RandomFill();
+
+                a.Add(b);
+
+                Console.WriteLine($"Akt. Größe: {initGroesse} und Laufzeit: {a.Laufzeit}");
+                if (a.Laufzeit.TotalMilliseconds - minuten * 60 * 1000 > 1000)
+                {
+                    intervall /= 2;
+                    initGroesse = initGroesse - intervall;
+                }
+                else if (a.Laufzeit.TotalMilliseconds - minuten * 60 * 1000 < 500)
+                {
+                    intervall *= 2;
+                    initGroesse = initGroesse + intervall;
+                }
+                Console.WriteLine($"Akt. Größe: {initGroesse}");
+
+
+            } while ((a.Laufzeit.TotalMilliseconds - minuten * 60 * 1000 > 1000) || (a.Laufzeit.TotalMilliseconds - minuten * 60 * 1000 < 500));
+            
+            return initGroesse;
+        }
+
+    }
+}
---
+++ Blatt1/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blatt1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int initGroesse = 10000;
+            initGroesse = Aufgabe3Matrix.LaufzeitErmittlungAdd(1, initGroesse);
+            Console.WriteLine($"Größe bei 1 min: {initGroesse}");
+            initGroesse = Aufgabe3Matrix.LaufzeitErmittlungAdd(2, initGroesse);
+            Console.WriteLine($"Größe bei 2 min: {initGroesse}");
+            initGroesse = Aufgabe3Matrix.LaufzeitErmittlungAdd(5, initGroesse);
+            Console.WriteLine($"Größe bei 5 min: {initGroesse}");
+            initGroesse = Aufgabe3Matrix.LaufzeitErmittlungAdd(10, initGroesse);
+            Console.WriteLine($"Größe bei 10 min: {initGroesse}");
+
+            Aufgabe3Matrix a = new Aufgabe3Matrix(1000, 1000);
+            a.RandomFill();
+            //a.Print();
+
+            Console.WriteLine();
+            Aufgabe3Matrix b = new Aufgabe3Matrix(1000, 1000);
+            b.RandomFill();
+            //b.Print();
+
+            Console.WriteLine();
+            Aufgabe3Matrix c = a.Add(b);
+            //c.Print();
+
+            //Console.WriteLine();
+            //Aufgabe3Matrix d = a.Mult(b);
+            //d.Print();
+
+            //Primzahlen(100);
+
+            //var a = 800000000;
+            //var b = 400000000;
+            //Console.WriteLine($"Für a = {a} und b = {b}");
+            //Console.WriteLine($"ggT: {GroessterGemeinsamerTeiler(a, b)}");
+            //Console.WriteLine($"kgV: {KleinsterGemeinsamesVielfache(a, b)}");
+            //Console.WriteLine($"ggT Rekursive: {GroessterGemeinsamerTeilerRekursive(a, b)}");
+        }
+
+        /*
+         * Euklidische Algorithmus
+         */
+        public static int GroessterGemeinsamerTeiler(int a, int b)
+        {
+            while (b > 0)
+            {
+                var rest = a % b;
+                a = b;
+                b = rest;
+            }
+            return a;
+        }
+
+        public static int GroessterGemeinsamerTeilerRekursive(int a, int b)
+        {
+            if (b <= 0)
+            {
+                return a;
+            }
+            var rest = a % b;
+            a = b;
+            b = rest;
+            return GroessterGemeinsamerTeilerRekursive(a, b);
+        }
+
+        /*
+         * z.B. wenn a und b Teilerfremd sind ist ggT 1 und dadurch ist das kgV das Produkt von a und b.
+         */
+        public static int KleinsterGemeinsamesVielfache(int a, int b)
+        {
+            return a * b / GroessterGemeinsamerTeiler(a, b);
+        }
+
+        public static void DruckTabelle(List<int> tabelle, int k)
+        {
+            for (int i = 1; i <= k; i++)
+            {
+                if (tabelle.Contains(i))
+                {
+                    Console.Write($"{i} \t");
+                }
+                else
+                {
+                    Console.Write("\t");
+                }
+
+                Console.Write('|');
+                if (i % 10 == 0)
+                {
+                    Console.WriteLine();
+                    for (int j = 0; j < 81; j++)
+                    {
+                        Console.Write('-');
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        public static void Primzahlen(int k)
+        {
+            List<int> zahlen = new List<int>();
+            for (int i = 2; i <= k; i++)
+            {
+                zahlen.Add(i);
+            }
+
+            DruckTabelle(zahlen, k);
+
+            for (int i = 2; i <= k; i++)
+            {
+                if (zahlen.Contains(i))
+                {
+                    for (int j = zahlen.FindIndex((x) => x == i)+1; j < zahlen.Count; j++)
+                    {
+                        if (zahlen[j] % i == 0)
+                        {
+                            zahlen.RemoveAt(j);
+                        }
+                    }
+
+                }
+            }
+
+            Console.WriteLine();
+            DruckTabelle(zahlen, k);
+        }
+
+        public static int Fibonacci(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+
+            if (n >= 3)
+            {
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+            }
+
+            return 1;
+        }
+    }
+}
