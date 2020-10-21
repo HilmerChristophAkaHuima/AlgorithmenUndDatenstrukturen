b1bccb3cde16197447b2407a9997e99267fd29fc
Christoph Hilmer <hilmerchristoph@googlemail.com>
2020-10-22 00:27:10 +0200

    Blatt 2 und Aufgabe 1

---
+++ Blatt2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Blatt2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+            Console.WriteLine($"Summe aller Zahlen von 1 bis {n}: {Aufgabe1(n)}");
+        }
+
+        public static int Aufgabe1(int n)
+        {
+            int summe = 0;
+            while (n > 0)
+            {
+                summe += n;
+                n--;
+            }
+
+            return summe;
+        }
+    }
+}
